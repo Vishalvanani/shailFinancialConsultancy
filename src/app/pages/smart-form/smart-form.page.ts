cec42a2e805a4059cdc9b04259cf390f391a821e
sandip moradiya <sandipmoradiya1996@gmail.com>
2022-12-28 20:04:24 +0530

    - form validation and return data to parent page

--- src/app/pages/smart-form/smart-form.page.ts
+++ src/app/pages/smart-form/smart-form.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { AlertService } from 'src/app/provider/alert.service';
 
 @Component({
   selector: 'app-smart-form',
@@ -7,8 +8,14 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./smart-form.page.scss'],
 })
 export class SmartFormPage implements OnInit {
-
-  constructor(public modalCtrl: ModalController) { }
+note: string = '';
+type: any;
+amount: any;
+category: any;
+  constructor(
+    public modalCtrl: ModalController,
+    public alertService: AlertService,
+    ) { }
 
   ngOnInit() {
   }
@@ -16,5 +23,24 @@ export class SmartFormPage implements OnInit {
   async closeModel() {
     const close: string = "Modal Removed";
     await this.modalCtrl.dismiss(close);
+  }
+   async submitModel() {
+    if(!this.category){
+      this.alertService.presentToast('Please select category')
+      return false;
+    } else if(!this.amount){
+      this.alertService.presentToast('Please enter amount')
+      return false;
+    }else {
+      let data = {
+        category: this.category, 
+        type: this.type, 
+        amount: this.amount, 
+        note: this.note
+      }
+      console.log(data);
+      await this.modalCtrl.dismiss(data);
+      return true;
+    }
   }
 }
