db8ba240d038a5a39168a5412bfcd1d8bf799e26
sandip moradiya <sandipmoradiya1996@gmail.com>
2023-03-01 23:52:49 +0530

    add income/expence api integrations

--- src/app/pages/smart-form/smart-form.page.ts
+++ src/app/pages/smart-form/smart-form.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController, NavParams } from '@ionic/angular';
+import * as moment from 'moment';
 import { AlertService } from 'src/app/provider/alert.service';
+import { CommonService } from 'src/app/provider/common.service';
 import { HttpService } from 'src/app/provider/http.service';
 
 @Component({
@@ -21,6 +23,7 @@ export class SmartFormPage implements OnInit {
     public modalCtrl: ModalController,
     public alertService: AlertService,
     private httpService: HttpService,
+    private commonService: CommonService,
     public navParams: NavParams,
   ) { }
 
@@ -57,13 +60,40 @@ export class SmartFormPage implements OnInit {
     if(errorMsg) {
       this.alertService.presentAlert(errorMsg);
     } else {
-      var data = {
-        category: this.category,
-        type: this.type,
-        amount: this.amount,
-        note: this.note
+      if (this.type === 'income') {
+        let income = {
+          client_id: this.commonService.userData.e_id,
+          income_id: this.category,
+          amount: this.amount,
+          date: moment(new Date()).format('YYYY-MM-DD')
+        };
+        this.alertService.presentLoader('');
+        this.httpService.post("add_customer_income.php", income).subscribe(res => {
+          this.alertService.dismissLoader();
+          this.modalCtrl.dismiss();
+        }, async (err) => {
+          this.alertService.dismissLoader();
+          this.modalCtrl.dismiss();
+          this.alertService.presentAlert(err);
+        })
+      } else {
+        let expence = {
+          c_client_id: this.commonService.userData.e_id,
+          c_income_id: this.category,
+          c_amount: this.amount,
+          c_date: moment(new Date()).format('YYYY-MM-DD')
+        };
+
+        await this.alertService.presentLoader('');
+        this.httpService.post("add_customer_expense.php", expence).subscribe( res => {
+           this.alertService.dismissLoader();
+           this.modalCtrl.dismiss();
+        }, async (err) => {
+           this.alertService.dismissLoader();
+           this.modalCtrl.dismiss();
+           this.alertService.presentAlert(err);
+        })
       }
-      await this.modalCtrl.dismiss({ data: data, mode: this.mode, index: this.index });
     }
   }
 
@@ -71,7 +101,6 @@ export class SmartFormPage implements OnInit {
   fetchIncomeCategory() {
     return new Promise((resolve, reject) => {
       this.httpService.get("list_income.php").subscribe(res => {
-        console.log('res: ', res);
         this.incomeCategoryList = res.items;
         resolve('');
       }, (err) => {
@@ -85,7 +114,6 @@ export class SmartFormPage implements OnInit {
   fetchExpenseCategory() {
     return new Promise((resolve, reject) => {
       this.httpService.get("list_expense.php").subscribe(res => {
-        console.log('res: ', res);
         this.expenseCategoryList = res.items;
         resolve('');
       }, (err) => {
